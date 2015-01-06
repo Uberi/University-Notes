942a4293e4e84b7c69176eed678c2331995bfbbe
Uberi <azhang9@gmail.com>
2015-01-06 17:38:34 -0500

    Add SCI267 and fix build script, plus update the index page.

--- build.sh
+++ build.sh
@@ -11,7 +11,6 @@ echo "$files" | while read file; do
   else
     difference=$(($(date -r "$file" +%s)-$(date -r "$result" +%s)))
   fi
-  difference=1
   
   if [ $difference -gt 0 ]; then # source file was modified after the output file or output doesn't exist
     echo "Rebuilding $file"
